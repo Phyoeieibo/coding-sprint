5e0bcae6461ab04bd9091174e50600fe5295bb2b
Phyoeieibo <phyoei083@gmail.com>
2018-06-25 11:24:14 +0630

    Learn %d %s %r

--- ex6.py
+++ ex6.py
@@ -7,8 +7,14 @@ print(y)
 print("I said: %r." % x)
 print("I also said: '%s'." % y)
 hilarious = False
-joke_evaluation = "isn't that joke so funny?! %r"
+joke_evaluation = "isn't that joke so funny?! % r"
 print(joke_evaluation % hilarious)
 w = "This is the left side of..."
 e = "a string with a right side."
-print(w+e)
+print(w + e)
+
+
+
+
+
+
