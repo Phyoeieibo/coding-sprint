1f28003b7b58ea4928b496df455c1f4d253de60f
Phyoeieibo <phyoei083@gmail.com>
2018-06-25 17:43:36 +0630

    Try and import random

--- ex41.py
+++ ex41.py
@@ -8,7 +8,7 @@ WORDS = []
 PHRASES = {
         "class %%%(%%%):":
          "Make a class named %%% that is-a %%%.",
-        "class %%%(object):\n\tdef __init__(self, ***)" :
+        "class %%%(object):\n\tdef __init__(self, ***)":
          "class %%% has-a __init__ that takes self and *** params.",
         "class %%%(object):\n\tdef ***(self, @@@)":
          "class %%% has-a function *** that takes self and @@@ params.",
