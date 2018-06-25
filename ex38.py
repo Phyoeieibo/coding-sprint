8fe780a58bc435aff4f11ca4d15972e7fcc5e3a3
Phyoeieibo <phyoei083@gmail.com>
2018-06-25 17:38:09 +0630

    List and pop()

--- ex38.py
+++ ex38.py
@@ -16,7 +16,10 @@ print("There we go: ", stuff)
 print("Let's do some things with stuff.")
 
 print(stuff[1])
-print(stuff[-1]) # whoa! fancy
+print(stuff[-1])
+# whoa! fancy
 print(stuff.pop())
-print(' '.join(stuff)) # what? cool!
-print('#'.join(stuff[3:5])) # super stellar!
+print(' '.join(stuff))
+# what? cool!
+print('#'.join(stuff[3:5]))
+# super stellar!
