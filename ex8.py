607290e3c6f01ba5bd4c26853de480c88d7243b1
Phyoeieibo <phyoei083@gmail.com>
2018-06-25 16:25:20 +0630

    Printing and Using formatter

--- ex8.py
+++ ex8.py
@@ -4,5 +4,3 @@ print(formatter % ("one", "two", "three", "four"))
 print(formatter % (True, False, False, True))
 print(formatter % (formatter, formatter, formatter, formatter))
 print(formatter % ("I had this thing.", "That you could type up right.", "But it didn't sing.", "So I said goodnight."))
-
-
